44af91e7d24c23bbbad30d24164481d4bad47b87
unknown <sanjayparasuraman90@gmail.com>
2026-03-09 12:13:53 +0530

    DAY 0 comittment start

--- HelloSanjay.java
+++ HelloSanjay.java
@@ -1,9 +1,10 @@
-package Sanjay-java-DSA-2026;
+package Sanjayjava2026;
 
 public class HelloSanjay {
     
 
 	public static void main(String[] args) {
 		System.out.println("Sanjay committing to no excuses. Starting today ");
+	}
     
 }
