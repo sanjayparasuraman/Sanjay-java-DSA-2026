b1a44f9768b9f220c223d661c0e2525406a3ec2b
unknown <sanjayparasuraman90@gmail.com>
2026-03-10 16:31:33 +0530

    leet code problem 1.Two sum solved

--- LeetCode_problems/Twosum.java
+++ LeetCode_problems/Twosum.java
@@ -1,14 +1,13 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int size = nums.length;
-        for(int i = 0;i<size;i++){
-            for(int j=i+1;j<size;j++){
-                if(nums[i]+nums[j] == target){
-                    return new int[]{i ,j};
-                }
-            }
+     HashMap <Integer , Integer> map  = new HashMap <>();
+     for(int i=0;i<nums.length;i++){
+        int needed = target - nums[i];
+        if(map.containsKey(needed)){
+            return new int[]{map.get(needed),i};
         }
-        return null ;
-        
-    }
+        map.put(nums[i],i);
+     }
+     return new int[]{};
+    } 
 }
