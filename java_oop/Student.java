c2724fdd62f407066817b192ed1b314ee4439034
unknown <sanjayparasuraman90@gmail.com>
2026-03-10 13:17:57 +0530

    Student class created as Blueprint

--- java_oop/Student.java
+++ java_oop/Student.java
@@ -11,15 +11,6 @@ public class Student {
         System.out.println("Age: " + age);
         System.out.println("Roll Number: " + rollNumber);
     }
-
-    public static void main(String[] args) {
-
-        Student s1 = new Student();
-
-        s1.name = "fempto";
-        s1.age = 19;
-        s1.rollNumber = 199;
-
-        s1.displayInfo();
-    }
 }
+
+   
