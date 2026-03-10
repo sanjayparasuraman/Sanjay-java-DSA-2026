c27a2e2e0260ef5b718cc61c2d7fcb2a96037c08
unknown <sanjayparasuraman90@gmail.com>
2026-03-10 13:16:06 +0530

    object created for Student class

---
+++ java_oop/Main.java
@@ -0,0 +1,25 @@
+package java_oop;
+
+public class Main {
+    
+
+	public static void main(String[] args) {
+		Student m1 = new Student();
+		m1.name = "zodd";
+		m1.rollnumber = 919;
+		m1.marks = 85.5;
+		m1.displayinfo();
+		
+		Student m2 = new Student();
+		m2.name = "guts";
+		m2.rollnumber = 818;
+		m2.marks = 95.9;
+		m2.displayinfo();
+		
+	
+	}
+
+}
+
+    
+
