ca26dbe85f3240dff3034d1b2a34a1fec0b14e08
unknown <sanjayparasuraman90@gmail.com>
2026-03-10 13:32:49 +0530

    Student class created in java oop

--- java_oop/Student.java
+++ java_oop/Student.java
@@ -2,14 +2,48 @@ package java_oop;
 
 public class Student {
 
-    String name;
-    int age;
-    int rollNumber;
+    private String name;
+    private int age;
+    private int rollNumber;
 
-    void displayInfo() {
+    public Student(String name, int age, int rollNumber) {
+        this.name = name;
+        this.age = age;
+        this.rollNumber = rollNumber;
+    }
+
+    public void displayInfo() {
         System.out.println("Name: " + name);
         System.out.println("Age: " + age);
         System.out.println("Roll Number: " + rollNumber);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public int getRollNumber() {
+        return rollNumber;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setRollNumber(int rollNumber) {
+        this.rollNumber = rollNumber;
+    }
+
+    public static String getCollegeName() {
+        return "Anna University";
     }
 }
 
