5f15a5d6d5fd06ec0c7b093494c87280fd43a10d
unknown <sanjayparasuraman90@gmail.com>
2026-03-10 12:43:39 +0530

    Day 1 java opp basics

--- java_opp/student.java
+++ java_opp/student.java
@@ -1,5 +1,25 @@
 package java_opp;
 
 public class Student {
-    
+
+    String name;
+    int age;
+    int rollNumber;
+
+    void displayInfo() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Roll Number: " + rollNumber);
+    }
+
+    public static void main(String[] args) {
+
+        Student s1 = new Student();
+
+        s1.name = "fempto";
+        s1.age = 19;
+        s1.rollNumber = 199;
+
+        s1.displayInfo();
+    }
 }
