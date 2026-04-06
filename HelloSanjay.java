8558c7683ef1e8e4918dc9314b6cc42888189eec
unknown <sanjayparasuraman90@gmail.com>
2026-04-06 19:31:28 +0530

    commit changes

--- HelloSanjay.java
+++ HelloSanjay.java
@@ -1,8 +1,5 @@
-package Sanjayjava2026;
-
 public class HelloSanjay {
     
-
 	public static void main(String[] args) {
 		System.out.println("Sanjay committing to no excuses. Starting today ");
 	}
